fe507a147a6aad32db31ee84e6c25e6eeb68f35e
amzedauren <dauren.amze1213@gmail.com>
2017-11-01 13:58:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ n = input("enter number\n")
 url = "https://thenounproject.com/search/json/icon/?q=" + searchItem + "&page=1&limit=" + n + "&raw_html=false"
 resp = json.loads(urllib.request.urlopen(url).read())
 
-directory = searchItem +  '{0:%H-%M-%S}'.format(datetime.datetime.now())
+directory = searchItem + '{0:%H-%M-%S}'.format(datetime.datetime.now())
 os.makedirs(directory)
 
 i = 0
